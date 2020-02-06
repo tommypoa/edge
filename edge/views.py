8800a8910e9bd2379fde1dabcf737b6788b9ea32
Tommy Poa <tommypoa@berkeley.edu>
2020-02-06 14:41:23 -0800

    Initializing. Current status - 2 canvas with background images.

---
+++ edge/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    # image_url = <connection to server to get NCAP images>. Local for now.
+    image_list = [("edge/CAP_DOS_2_CAR_0024__0081.jpg","im1"), ("edge/CAP_DOS_2_CAR_0024__0082.jpg", "im2")]
+    context = {
+        'image_list': image_list,
+    }
+    return render(request, 'edge/index.html', context)
