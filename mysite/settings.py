91df9f5fcc06e344b20b12776b887c807e19c219
Tommy Poa <tommypoa@berkeley.edu>
2020-02-24 23:05:48 -0800

    Static root fix.

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,10 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-# STATIC_URL = '/static/'
+PROJECT_ROOT   =   os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static')
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
