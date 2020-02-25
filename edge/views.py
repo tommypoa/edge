e61afd0556eeb14085b48f27a608d34630495519
Tommy Poa <tommypoa@berkeley.edu>
2020-02-24 19:03:38 -0800

    Saving multiple points implemented.

--- edge/views.py
+++ edge/views.py
@@ -19,61 +19,48 @@ def index(request):
     return render(request, 'edge/index.html', context)
 
 def save(request):
-    response_data = {}
+    im1_name = request.POST.get('im1_name')
+    im2_name = request.POST.get('im2_name')
+    length = request.POST.get('numPoints')
 
-    if request.is_ajax():
-        im1_name = request.POST.get('im1_name')
-        im2_name = request.POST.get('im2_name')
-        im1_x = request.POST.get('im1_x')
-        im1_y = request.POST.get('im1_y')
-        im2_x = request.POST.get('im2_x')
-        im2_y = request.POST.get('im2_y')
-        
-        response_data['im1_name'] = im1_name
-        response_data['im2_name'] = im2_name
-        response_data['im1_x'] = im1_x
-        response_data['im1_y'] = im1_y
-        response_data['im2_x'] = im2_x
-        response_data['im2_y'] = im2_y
-
+    for pointNum in range(int(length)):
+        pt = request.POST.getlist('points[' + str(pointNum) + '][]')
         Coordinate.objects.create(
             im1_name = im1_name,
             im2_name = im2_name,
-            im1_x = im1_x,
-            im1_y = im1_y,
-            im2_x = im2_x,
-            im2_y = im2_y,
+            im1_x = pt[0],
+            im1_y = pt[1],
+            im2_x = pt[2],
+            im2_y = pt[3],
             created_at = timezone.now()
         )
-        return JsonResponse(response_data)
-    else:
-        return HttpResponseRedirect(reverse('edge:index'))
+    return JsonResponse({})
 
 def visualize(request):
-    logger = logging.getLogger(__name__)
-    file = 'edge/world/test.points'
-    pts0 = request.POST.getlist('points[0][]')
-    pts1 = request.POST.getlist('points[1][]')
+    file = 'edge/world/df_format.points'
     df = pd.read_csv(file)
-    df = df.append(pd.Series(pts0, index=df.columns), ignore_index=True)
-    df = df.append(pd.Series(pts1, index=df.columns), ignore_index=True)
-    pts0 = df.loc[:, ['mapX', 'mapY']].values
-    pts1 = df.loc[:, ['pixelX', 'pixelY']].values
+
+    length = request.POST.get('numPoints')
+    for pointNum in range(int(length)):
+        pt = request.POST.getlist('points[' + str(pointNum) + '][]')
+        df = df.append(pd.Series(pt, index=df.columns), ignore_index=True)
+    im1 = df.loc[:, ['mapX', 'mapY']].values
+    im2 = df.loc[:, ['pixelX', 'pixelY']].values
 
     trans, _ = cv.estimateAffinePartial2D(
-        np.float32(pts1), np.float32(pts0),
+        np.float32(im2), np.float32(im1),
         method=cv.LMEDS)
 
-    img0_file = 'edge/static/edge/CAP_DOS_2_CAR_0024__0081.jpg'
-    img1_file = 'edge/static/edge/CAP_DOS_2_CAR_0024__0082.jpg'
-    img0 = cv.imread(img0_file, cv.IMREAD_GRAYSCALE)
+    img1_file = 'edge/static/edge/CAP_DOS_2_CAR_0024__0081.jpg'
+    img2_file = 'edge/static/edge/CAP_DOS_2_CAR_0024__0082.jpg'
     img1 = cv.imread(img1_file, cv.IMREAD_GRAYSCALE)
+    img2 = cv.imread(img2_file, cv.IMREAD_GRAYSCALE)
 
     # fit img1 onto img0
-    img_overlay = cv.warpAffine(src=img1, M=trans, dsize=img0.shape[::-1])
+    img_overlay = cv.warpAffine(src=img2, M=trans, dsize=img1.shape[::-1])
     # overlay with 50% transparency
-    img_overlay = cv.addWeighted(img0, 0.5, img_overlay, 0.5, gamma=0.0)
-    output_url = 'edge/static/edge/visualization.jpg'
+    img_overlay = cv.addWeighted(img1, 0.5, img_overlay, 0.5, gamma=0.0)
+    output_url = 'edge/static/edge/last_visualization.jpg'
     cv.imwrite(output_url, img_overlay)
     response_data = {'url': output_url[4:]}
     return JsonResponse(response_data)
